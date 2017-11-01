3d0f5c164bdc122a4975670e14bea26a76086435
Elaine Chan <elaine.chan@outlook.com>
2017-10-31 21:38:11 -0400

    jquery handler

--- 002/cat-carousel.js
+++ 002/cat-carousel.js
@@ -16,9 +16,11 @@ event.which
 function handleIconClicks() {
     $('.thumbnail').on('click', event => {
         const targetThumbnail = $(event.currentTarget);
-        const otherThumbnails = $('.thumbnail').not(targetThumbnail);
-        $('.hero').html(targetThumbnail);
-        currentTarget.toggleClass('hero');
+        const otherThumbnails = $('thumbnail').not(targetThumbnail);
+        $('.hero').empty();
+        $(targetThumbnail).clone().appendTo('.hero').removeClass('thumbnail');
+        otherThumbnails.removeClass('hero')
+        otherThumbnails.addClass('thumbnail');
     })
 }
 
