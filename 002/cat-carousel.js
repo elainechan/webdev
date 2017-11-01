4e00c03c977a101c2759f6ff6a9179ad04950145
Elaine Chan <elaine.chan@outlook.com>
2017-10-31 21:46:32 -0400

    removed useless comments

--- 002/cat-carousel.js
+++ 002/cat-carousel.js
@@ -1,18 +1,4 @@
-/*
-user selects a thumbnail using the mouse or keyboard
-image should be displayed in the full size image container at the top 
-Updating the image should also update its alt attribute. 
-Tab to navigate thumbnails
-Enter key to select
-*/
-/*
-event.currentTarget
-.click()
-event.which
-.keydown()
-.toggleClass()
-*/
-
+// REPL at https://repl.it/Icy2/1997
 function handleIconClicks() {
     $('.thumbnail').on('click', event => {
         const targetThumbnail = $(event.currentTarget);
