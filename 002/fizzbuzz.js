c6bd2255e69585f8f7b1f8cf3e325f4a6b5e13e5
Elaine Chan <elaine.chan@outlook.com>
2017-11-01 12:09:30 -0400

    fizzbuzz function

--- 002/fizzbuzz.js
+++ 002/fizzbuzz.js
@@ -1,12 +1,30 @@
-function handleFizzBuzz(n) {
+function handleFizzBuzz() {
   $('#number-chooser').submit(event => {
     event.preventDefault();
+    $('.js-results').empty();
     let chosenNumber = $('#number-choice').val();
-    for (let i = 0; i <= chosenNumber; i++) {
-      $('.js-results').append('<div class="fizz-buzz-item"><span></span></div>');
-      $('.fizz-buzz-item span').text(i);
+    let outcomes = fizzBuzz(chosenNumber);
+    for (let i = 0; i < outcomes.length; i++) {
+      $('.js-results').append(`<div class="fizz-buzz-item"><span>${outcomes[i]}</span></div>`);
+      $('div:contains('fizz')').addClass('fizz');
     }
-  })
+  });
+}
+
+function fizzBuzz(countTo) {
+  let a = [];
+  for (let i = 1; i <= countTo; i++) {
+    if (i % 15 === 0) {
+      a.push("fizzbuzz");
+    } else if (i % 3 === 0) {
+      a.push("fizz");
+    } else if (i % 5 === 0) {
+      a.push("buzz");
+    } else {
+      a.push(i);
+    }
+  }
+  return a;
 }
 
 $(handleFizzBuzz);
