057ca4ca7b9b469a8ad59c6b99201fafe021b163
Elaine Chan <elaine.chan@outlook.com>
2017-11-01 16:43:57 -0400

    bubble sort works

--- 003/bubble-sort.js
+++ 003/bubble-sort.js
@@ -1,11 +1,11 @@
 function bubbleSort(arr) {
-    for (let i = 0; i < arr.length - 1; i++) {
-        let curr = arr[i];
-        if (arr[i] > arr[i + 1]) {
-            arr[i] = arr[i + 1];
-            curr = arr[i + 1];    
-        } else {
-            curr = arr[i + 1];
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length - i; j++) {
+            let temp = arr[j];
+            if (arr[j] > arr[j + 1]) {
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
         }
     }
     return arr;
