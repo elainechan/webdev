b04b53c79eeb686e60e5d972f2fb7f5a0cb23cf4
Elaine Chan <elaine.chan@outlook.com>
2017-11-24 16:18:31 -0500

    function to chnage image URL

--- quiz/main.js
+++ quiz/main.js
@@ -373,6 +373,13 @@ function setHandleRestartButton() {
     });
 }
 
+function getImgURL(str) { // takes filename as input
+    let root = `https://github.com/elainechan/webdev/blob/master/quiz/`;
+    let file = str.replace(/^[.(?=/)]/, root); // replace dot with url root
+    let url = file.concat(`?raw=true`); // concat url with retrieve code
+    return url;
+}
+
 /* todo: optional redesign
 function initialize() {
     // only called once
