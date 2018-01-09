774721e123ad4690fbf4a7f46b3bd670f0ac8cec
elainechan <elaine.chan@outlook.com>
2018-01-08 22:25:49 -0500

    Uses YouTube Data API to perform video search

---
+++ tubefinder/README.md
@@ -0,0 +1,11 @@
+# Making an API Call
+
+## Steps
+1. Attempt to authenticate through Postman or cURL
+2. Save both request and response to a document
+	* Full JSON examinable in static doc, not just a Postman request
+3. Get in-page JS to send requests successfully 
+	* `console.log()` full API response to a pre-baked request
+	* depending on depth, this request may be incomplete
+4. Add in-page JS for dynamic response
+5. Build HTML based on that response
---
+++ tubefinder/main.js
@@ -0,0 +1,40 @@
+const YOUTUBE_SEARCH_URL = 'https://www.googleapis.com/youtube/v3/search';
+
+function getDataFromApi(searchTerm, callback) {
+  const query = {
+	part: 'snippet',
+	key: 'AIzaSyBkdN6yb7KKtlV3h89IY1rMAD92FZo78jw', // TODO: remove before git push
+	q: `${searchTerm} in:name`,
+  };
+  $.getJSON(YOUTUBE_SEARCH_URL, query, callback);
+}
+
+function renderResult(result) { // TODO: result is undefined
+  return `
+	<div>
+	  <a href="https://www.youtube.com/watch?v=${result.id.videoId}" target="_blank"><img src="${result.snippet.thumbnails.default.url}" alt="YouTube video snippet"></a>
+      <h2>
+      <a class="js-result-name" href="https://www.youtube.com/watch?v=${result.id.videoId}" target="_blank">${result.snippet.title}</a> by <a class="js-user-name" href="https://www.youtube.com/channel/${result.snippet.channelId}" target="_blank">${result.snippet.channelTitle}</a></h2>
+      <p>Publishing date: <span class="js-watchers-count">${result.snippet.publishedAt}</span></p>
+      <p>Description: <span class="js-issues-count">${result.snippet.description}</span></p>
+    </div>
+  `;
+}
+
+function displayYouTubeSearchData(data) {
+  const results = data.items.map((item, index) => renderResult(item)); // TODO: item is undefined
+  $('.js-search-results').html(results);
+}
+
+function watchSubmit() {
+  $('.js-search-form').submit(event => {
+    event.preventDefault();
+    const queryTarget = $(event.currentTarget).find('.js-query');
+    const query = queryTarget.val();
+    // clear out the input
+    queryTarget.val("");
+    getDataFromApi(query, displayYouTubeSearchData);
+  });
+}
+
+$(watchSubmit);
